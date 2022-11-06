565f5dcb93a5245c9fd9a0f529ab4f7e2a8106ce
Anirudh <anirudhshukla02@gmail.com>
2022-11-06 16:10:12 +0530

    synced navbar links with the route changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -10,8 +10,8 @@ export function Navbar() {
         <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
             <Container>
                 <Nav className='me-auto'>
-                    <Nav.Link to={"/"} as={NavLink}>Home</Nav.Link>
-                    <Nav.Link to={"/store"} as={NavLink}>Store</Nav.Link>
+                    <Nav.Link to={"/Home"} as={NavLink}>Home</Nav.Link>
+                    <Nav.Link to={"/"} as={NavLink}>Store</Nav.Link>
                     <Nav.Link to={"/about"} as={NavLink}>About</Nav.Link>
                 </Nav>
                 <Form className="d-flex p-2">
