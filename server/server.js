07c56cb093ffb45f6bf0a9881be4c475a120ced1
Anirudh <anirudhshukla02@gmail.com>
2022-12-01 16:25:35 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONT_END_URL
+}))
 
 app.use('/', express.static(__dirname + '/public'))
 app.use('/api', require('./route/api'))
